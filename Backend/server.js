5def92b3cc6ea983a2317ba9c41f2b430ad8fc54
Sohail Usman <sohailgamming112233>
2026-08-28 18:51:55 +0500

    Prepare backend for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,6 @@
-import dns from "dns";
-
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
 import connectDB from "./config/db.js";
 
 import authRoutes from "./routes/authRoutes.js";
@@ -19,9 +14,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -32,17 +25,15 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
-
 app.use("/api/products", productRoutes);
-
 app.use("/api/cart", cartRoutes);
-
 app.use("/api/address", addressRoutes);
-
 app.use("/api/orders", orderRoutes);
 
 connectDB();
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
